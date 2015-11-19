5fe7e3625fe55eb35e0eca4d3c57d879c735e8cb
Me <Flavian@flavs-macbook-pro.local>
2015-11-19 12:31:49 +0000

    Initial Commit

---
+++ Contacts/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Contacts
+//
+//  Created by flav Onu on 19/11/2015.
+//  Copyright © 2015 Flavian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Contacts/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Contacts
+//
+//  Created by flav Onu on 19/11/2015.
+//  Copyright © 2015 Flavian. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
